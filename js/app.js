502205e5bf0d15069c03dc7071ba4fce2a7c3642
Nathan Lee & Sebastien Cheraux <pair+naaaaate.sebsonic2o@devbootcamp.com>
2015-05-28 16:58:42 -0700

    Add column win

--- js/app.js
+++ js/app.js
@@ -12,7 +12,17 @@ var createNestedArray = function(first_dimension, second_dimension, default_valu
   return nestedArray;
 }
 
+// var c = function(array, item) {
+//   var count = 0;
+//   for(var i = 0; i < array.length; i++){
+//       if(array[i] == item)
+//           count++;
+//   }
+//   return count;
+// }
+
 var Game = function (){
+  this.win = false;
   this.color = "yellow";
   this.columns = createNestedArray(7, 6, 'X')
   // this.player2
@@ -33,21 +43,33 @@ Game.prototype.changeColor = function() {
 }
 
 Game.prototype.checkColumn = function(column) {
-  //game checks board for 4 in a row.
+  var count = 0;
   var column_to_check = this.columns[column - 1];
 
   for (var index = 0; index < column_to_check.length ; index++) {
+
     if (column_to_check[index] == 'X') {
+      if (count == 3) {
+        this.win = true;
+        console.log("WIIIIIIIIIIIIN!!!");
+      }
       column_to_check[index] = this.color;
       return index+1;
     }
+    else if (column_to_check[index] == this.color)
+      count++;
+    }
+    else {
+      count = 0;
+    }
+
   }
 }
 
-Game.prototype.checkWin = function() {
-  //game checks board for 4 in a row.
-  //
-}
+// Game.prototype.checkWin = function() {
+//   //game checks board for 4 in a row.
+//   //
+// }
 
 Game.prototype.dropCoin = function(column) {
   //player drops coin returns a position on board
@@ -56,7 +78,7 @@ Game.prototype.dropCoin = function(column) {
   //place blue or yellow coin in that position..
   var color = this.color
   var row = this.checkColumn(column);
-  var win = this.checkWin();
+  // var win = this.checkWin();
 
   if (!win) {
     this.changeColor();
@@ -64,7 +86,7 @@ Game.prototype.dropCoin = function(column) {
 
   return { column: column,
     row: row,
-    win: win,
+    win: this.win,
     color: color
   };
 }
@@ -72,11 +94,13 @@ Game.prototype.dropCoin = function(column) {
 // game = new Game();
 
 // for (i=1; i<3; i++) {
+
 //   for (j=1; j<8; j++) {
-//     result = game.dropCoin(j);
+//     result = game.dropCoin(0);
 //     console.log(result["color"]);
 //     console.log(result["row"]);
 //   }
+
 // }
 
-// console.log(game.columns);
+console.log(game.columns);
