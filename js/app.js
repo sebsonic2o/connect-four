41c0b343b8785451623a3c83a0018597717d791b
Sebastien Cheraux <scheraux@gmail.com>
2015-05-28 21:48:30 -0700

    Add initial diagonal win

--- js/app.js
+++ js/app.js
@@ -68,13 +68,12 @@ Game.prototype.checkColumn = function(column) {
 
 Game.prototype.checkRow = function(row, column) {
   var count = 0;
-  var row_to_check = this.rows[row - 1];
+  var row_to_check = this.rows[row];
 
   // I fill the row to check with the color at the right position
-  row_to_check[column - 1] = this.color;
+  row_to_check[column] = this.color;
 
   for (var index = 0; index < row_to_check.length ; index++) {
-
     if (row_to_check[index] == this.color) {
       count++;
       if (count == 4) {
@@ -84,14 +83,14 @@ Game.prototype.checkRow = function(row, column) {
     else {
       count = 0;
     }
-
   }
 
+  return false;
 }
 
 Game.prototype.checkColumn = function(column) {
   var count = 0;
-  var column_to_check = this.columns[column - 1];
+  var column_to_check = this.columns[column];
 
   for (var index = 0; index < column_to_check.length ; index++) {
 
@@ -100,7 +99,7 @@ Game.prototype.checkColumn = function(column) {
         this.win = true;
       }
       column_to_check[index] = this.color;
-      return index+1;
+      return index;
     }
     else if (column_to_check[index] == this.color) {
       count++;
@@ -112,17 +111,84 @@ Game.prototype.checkColumn = function(column) {
   }
 }
 
+Game.prototype.checkDiagonals = function(row, column) {
+  var count = 0;
+
+  // left to right diagonal
+  // going up
+  var j = row+1;
+  for (var i = column+1; i < this.columns.length; i++) {
+    if (j >= this.columns[i].length || this.columns[i][j] != this.color) {
+      break;
+    }
+    else {
+      count++;
+    }
+    j++;
+  }
+  // going down
+  var j = row-1;
+  for (var i = column-1; i >= 0; i--) {
+    if (j < 0 || this.columns[i][j] != this.color) {
+      break;
+    }
+    else {
+      count++;
+    }
+    j--;
+  }
+
+  if (count == 3) {
+    return this.win = true;
+  }
+
+  // right to left diagonal
+  // going up
+  var j = row+1;
+  for (var i = column-1; i >= 0; i--) {
+    if (j >= this.columns[i].length || this.columns[i][j] != this.color) {
+      break;
+    }
+    else {
+      count++;
+    }
+    j++;
+  }
+  // going down
+  var j = row-1;
+  for (var i = column+1; i < this.columns.length; i++) {
+    if (j < 0 || this.columns[i][j] != this.color) {
+      break;
+    }
+    else {
+      count++;
+    }
+    j--;
+  }
+
+  if (count == 3) {
+    return this.win = true;
+  }
+
+  return false;
+}
+
 Game.prototype.dropCoin = function(column) {
+  column--;
   var color = this.color;
   var row = this.checkColumn(column);
 
   if (!this.win) {
     this.checkRow(row, column);
+  }
+
+  if (!this.win) {
+    this.checkDiagonals(row, column);
     this.changeColor();
   }
 
-  return { column: column,
-    row: row,
+  return { column: column+1,
+    row: row+1,
     win: this.win,
     color: color
   };
